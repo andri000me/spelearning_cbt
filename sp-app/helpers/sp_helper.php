f3eb91268cbe9da5e4a444588794b46462639c04
Supangatoy <Supangatoy@gmail.com>
2019-05-18 10:07:56 +0700

    add changelog git history

--- sp-app/helpers/sp_helper.php
+++ sp-app/helpers/sp_helper.php
@@ -15,3 +15,47 @@ function tgl_bilang($t,$time=0,$am=0){
 	}
 	return $hasil;
 }
+function getChangelog($n=20){
+	// Load Last 10 Git Logs
+	$git_history = [];
+	$git_logs = [];
+	exec("git log -".$n, $git_logs);
+	// Parse Logs
+	$last_hash = null;
+	foreach ($git_logs as $line)
+	{
+	    // Clean Line
+	    $line = trim($line);
+	    // Proceed If There Are Any Lines
+	    if (!empty($line))
+	    {
+	        // Commit
+	        if (strpos($line, 'commit') !== false)
+	        {
+	            $hash = explode(' ', $line);
+	            $hash = trim(end($hash));
+	            $git_history[$hash] = [
+	                'message' => ''
+	            ];
+	            $last_hash = $hash;
+	        }
+	        // Author
+	        else if (strpos($line, 'Author') !== false) {
+	            $author = explode(':', $line);
+	            $author = trim(end($author));
+	            $git_history[$last_hash]['author'] = $author;
+	        }
+	        // Date
+	        else if (strpos($line, 'Date') !== false) {
+	            $date = explode(':', $line, 2);
+	            $date = trim(end($date));
+	            $git_history[$last_hash]['date'] = strtotime($date);
+	        }
+	        // Message
+	        else {
+	            $git_history[$last_hash]['message'] .= $line ." ";
+	        }
+	    }
+	}
+	return $git_history;
+}
