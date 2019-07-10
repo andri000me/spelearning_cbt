5f7b4109d2d2f84d1f43f8f7d7531e81a43b2e98
Supangatoy <Supangatoy@gmail.com>
2019-07-10 21:23:06 +0700

    fixing reset button

--- sp-app/controllers/admin/Reset.php
+++ sp-app/controllers/admin/Reset.php
@@ -18,7 +18,6 @@ class Reset extends CI_Controller {
 		$data['guru']=$this->db->get('cbt_user')->result();
 		$this->load->view('head_meta',$data);
 		$this->load->view('admin/header',$data);
-		
 		$this->load->view('admin/reset',$data);
 		$this->load->view('admin/footer',$data);
 	}
@@ -31,6 +30,12 @@ class Reset extends CI_Controller {
 
 		if ($user == $GLOBALS['u']['Username'] AND $pwd == $GLOBALS['u']['Password']) {
 			if ($this->db->query('TRUNCATE "cbt_jawaban", "cbt_kelas", "cbt_mapel", "cbt_nilai", "cbt_paketmateri", "cbt_paketsoal", "cbt_pelajaran", "cbt_pesan", "cbt_siswa", "cbt_siswa_pelajaran", "cbt_siswa_ujian", "cbt_soal", "cbt_tanya_pelajaran", "cbt_ujian"')) {
+
+				$sq=["cbt_jawaban_Urutan_seq","cbt_kelas_Urut_seq","cbt_cleint_Urut_seq","cbt_mapel_Urut_seq","cbt_nilai_Urut_seq","cbt_paketmateri_Urut_seq","cbt_paketsoal_Urut_seq","cbt_pelajaran_Urut_seq","cbt_pesan_Urut_seq","cbt_siswa_pelajaran_Urut_seq","cbt_siswa_ujian_Urut_seq","cbt_siswa_Urut_seq","cbt_soal_Urut_seq","cbt_tanya_pelajaran_Urut_seq","cbt_tes_Urut_seq","cbt_ujian_Urut_seq"];
+				foreach ($sq as $k) {
+					$this->db->query('ALTER SEQUENCE "'.$k.'" RESTART WITH 1');
+				}
+				// die();
 				$this->m_config->pindah("admin/reset",1,'Sukses mereset');
 			} else {
 				$this->m_config->pindah("admin/reset",0,'Gagal reset silahkan ulangi lagi dan laporkan jika terjadi hal error berullang');
